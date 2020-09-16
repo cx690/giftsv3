8a4eabb5a96629e2eb36d8173b8c6dd16d199de1
caixue <1256953820@qq.com>
2020-09-16 14:47:21 +0800

    类型处理

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -12,9 +12,13 @@ export function addCommas(x: number | string) {
 	return arr[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,') + (arr.length > 1 ? '.' + arr[1] : '');
 }
 
-export function setValue(target:any,newValue:any){
-	if(typeof target === 'object' && typeof newValue === 'object'){
-		for(let key in newValue){
+interface Obj<T> {
+	[prop: string]: T
+}
+
+export function setValue<T>(target: Obj<T>, newValue: Obj<T>) {
+	if (typeof target === 'object' && typeof newValue === 'object') {
+		for (let key in newValue) {
 			target[key] = newValue[key];
 		}
 	}
