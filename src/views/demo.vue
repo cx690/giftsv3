9e9b37a9767ca40f48c173a9e396813fe655aed5
caixue <1256953820@qq.com>
2021-03-30 10:51:05 +0800

    升级版本

--- src/views/demo.vue
+++ src/views/demo.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="about">{{msg}}</div>
+    <div class="about">{{msg}}</div>
 </template>
 <script lang="ts">
 import { onMounted, ref, Ref, reactive, toRefs } from "vue";
 export default {
-  setup() {
-    const msg = ref("没想到吧你？哈哈");
-    return {
-      msg
-    };
-  }
+    setup() {
+        const msg = ref("没想到吧你？哈哈");
+        return {
+            msg,
+        };
+    },
 };
 </script>
